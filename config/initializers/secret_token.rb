53b4b0cd28c551f5de06f581b1f218449368791f
Michael Prouty <michael.prouty@gmail.com>
2013-10-25 22:13:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SweetFantasy::Application.config.secret_key_base = '023254281b3eaa85b4a1d57110a1b460c021186e2669613de496512c35bf4cc83b21af002b8537ac094a885f49a93a735a443ce5fbfecbc61f4e417d53728b66'
