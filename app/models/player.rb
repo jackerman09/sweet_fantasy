0c53f64354168e742c5c916ab9c08c1167512a1c
Michael Prouty <michael.prouty@gmail.com>
2013-11-04 17:00:13 -0500

    Add MatchUpVotes model and migration

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ActiveRecord::Base
   belongs_to :team
   has_many :injuries
+  has_many :won_votes, class_name: 'MatchUpVote', foreign_key: 'chosen_player_id'
+  has_many :lost_votes, class_name: 'MatchUpVote', foreign_key: 'unchosen_player_id'
 end
